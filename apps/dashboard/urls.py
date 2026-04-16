c8a184258bf714e73c6fa4951b127f0ac7151759
Romendesu <tutifrutihd4@gmail.com>
2026-04-16 14:44:53 +0200

    add schedule view

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('inicio/', views.home_redirect, name='dashboard-inicio'), 
-    path('horario/', views.home_redirect, name='dashboard-horario'), 
+    path('horario/', views.schedule_redirect, name='dashboard-horario'), 
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -48,3 +48,37 @@ def home_redirect(request):
     }
     
     return render(request, 'dashboard/home.html', context)
+
+# Redireccionamiento al Horario
+@login_required
+def schedule_redirect(request):
+    rol, profile = obtain_rol(request.user)
+    try:
+        locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+    except:
+        locale.setlocale(locale.LC_TIME, 'es-ES')
+    
+    now = datetime.now()
+    # Encontramos el lunes de la semana actual
+    actual_monday = now - timedelta(days=now.weekday())
+
+    # Creamos una lista con los números de los 7 días (Lunes a Domingo)
+    week_days = [(actual_monday + timedelta(days=i)).day for i in range(7)]
+
+    day = now.day
+    month_name = now.strftime("%B")
+    year = now.year
+    date_str = f"{now.strftime('%A').capitalize()}, {day} {month_name} {year}"
+
+    context = {
+        'Rol': rol,
+        'Perfil': profile,
+        'Title': f'Dashboard - {rol}',
+        'Date': date_str,
+        'day': day,
+        'month_name': month_name,
+        'year': year,
+        'semana_dias': week_days, 
+    }
+   
+    return render(request, 'dashboard/schedule.html', context)
